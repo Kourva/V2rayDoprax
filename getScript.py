08536272a24d95118c5d11e53cdd8c4d5ca60f65
Kourva <118578799+Kourva@users.noreply.github.com>
2023-02-11 03:24:02 +0330

    Get script for your cloudflare worker
    ! Doprax url needed

---
+++ getScript.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+
+# Get CloudFlare worker script by your Doprax url
+
+
+# Imports
+import time
+import sys
+import re
+
+
+# Script
+script = """
+addEventListener(
+
+    "fetch", event => {
+
+        let url = new URL(event.request.url);
+
+        url.hostname = "target";
+
+        url.protocol = "https";
+
+        let request = new Request(url, event.request);
+
+        event.respondWith(
+
+            fetch(request)
+
+        )
+    }
+)
+"""
+
+
+# Worker script
+def worker_script(doprax):
+    temp = re.sub("target", doprax, script)
+    return temp
+
+
+# Run the program
+if __name__ == "__main__":
+    if len(sys.argv) != 1:
+        if sys.argv[1] in ["-h", "--help"]:
+            print(
+                """
+Help: python getAllVless.py [arguments]
+
+    create Script -> python getScript [Doprax url without 'https://' and '/' at the end]
+    example: test.eu-gacagfhs.dopraxrocks.net
+"""
+            )
+        elif sys.argv[1]:
+            for char in worker_script(sys.argv[1]):
+                print(char, end="", flush=True)
+                time.sleep(0.01)
+        else:
+            print("Invalid argument! run with -h")
+    else:
+        print("No argument! run with -h")
+
+
+# EOF
