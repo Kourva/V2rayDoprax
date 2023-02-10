9fc915532468f7323dde5d654c70b3716d66ad94
Kourva <118578799+Kourva@users.noreply.github.com>
2023-02-11 03:20:54 +0330

    Get UUID version1 or version4

---
+++ getUUID.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+
+# Get both version1 or version4 UUID from https://www.uuidgenerator.net
+# GitHub: https://github.com/Kourva/getUUID
+
+
+# Imports
+import requests
+import time
+import sys
+import bs4
+
+
+# Gets version 1 UUID
+def get_version1():
+    response = requests.get("https://www.uuidgenerator.net/version1")
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
+    temp = soup.find(id="generated-uuid").text
+    return temp.upper()
+
+
+# Gets version 4 UUID
+def get_version4():
+    response = requests.get("https://www.uuidgenerator.net/version4")
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
+    temp = soup.find(id="generated-uuid").text
+    return temp.upper()
+
+
+# Takes arguments
+arguments = sys.argv
+
+
+# Help message
+help_message = """
+Help: python getUUID.py [[-h] [-v1] [-v4]]
+    -h  : Show this message and exit.
+    -v1 : get version 1 UUID
+    -v4 : get version 4 UUID
+
+* What is a Version 1 UUID?
+    A Version 1 UUID is a universally unique identifier that 
+    is generated using a timestamp and the MAC address of 
+    the computer on which it was generated.
+
+* What is a version 4 UUID?
+    A Version 4 UUID is a universally unique identifier that 
+    is generated using random numbers.using a secure random 
+    number generator.
+"""
+
+
+# Run the program
+if __name__ == "__main__":
+    if "-h" in arguments:
+        print(help_message)
+        sys.exit()
+
+    if "-v1" in arguments:
+        for char in "\033[2;34m* Getting version1 UUID for you...\033[m\n":
+            print(char, end="", flush=True)
+            time.sleep(0.01)
+        print(f"Version1 UUID: \033[2;32m{get_version1()}\033[m\n")
+
+    if "-v4" in arguments:
+        for char in "\033[2;34m* Getting version4 UUID for you...\033[m\n":
+            print(char, end="", flush=True)
+            time.sleep(0.01)
+        print(f"Version4 UUID: \033[2;32m{get_version4()}\033[m\n")
+
+    if len(sys.argv) == 1:
+        print("\033[2;31mError: no argument found! Use -h\033[m")
+
+# EOF
