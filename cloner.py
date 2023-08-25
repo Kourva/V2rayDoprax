448afa10912781f8dd76f7511fa0b53e01ca3ef5
Kourva <118578799+Kourva@users.noreply.github.com>
2023-08-25 06:45:50 -0400

    Update cloner.py

--- cloner.py
+++ cloner.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 # Proxy cloner: Clone your proxy to many proxies
 # Works with both vless & vmess proxies
@@ -6,17 +7,21 @@
 # Imports
 import random, base64, time, json, sys
 
-
 # Vless cloner
-def clone_vless(url):
+def clone_vless(url: str) -> None:
+
+    # Extract Vless URL information
     target = url.split("@")[1].split("?")[0]
     ipaddr, port = target.split(":")
     remark = url.split("#")[1]
 
+    # Get all IP ranges from file
     with open("ip/all_result.txt", "r") as file:
-        all_servers = file.readlines()
-        iplist = [random.choice(all_servers).split("\n")[0] for _ in range(20)]
+        iplist = [
+            random.choice(file.readlines()).split("\n")[0] for _ in range(20)
+        ]
 
+    # Create clones for vless urls
     clones = []
     for index, server in enumerate(iplist, start=1):
         tempvar = (
@@ -26,28 +31,31 @@ def clone_vless(url):
         )
         clones.append(tempvar)
 
+    # Save clones to results
     with open("data/vless_clones.txt", "a") as data:
         for clone in clones:
             data.write(clone)
 
-
 # Vmess cloner
-def clone_vmess(url):
+def clone_vmess(url: str) -> None:
+
+    # Extract Vmess URL information
     encoded_data = url.split("vmess://")[1].strip()
     output = json.loads(
         base64.b64decode(encoded_data).decode("utf-8").replace("'", '"')
     )
-
     ipaddr, port, remark = (
         output["port"],
         output["add"],
         output["ps"],
     )
 
+    # Get all IP ranges from file
     with open("ip/all_result.txt", "r") as file:
         all_servers = file.readlines()
         iplist = [random.choice(all_servers).split("\n")[0] for _ in range(20)]
 
+    # Create clones for vmess urls
     clones = []
     for index, server in enumerate(iplist, start=1):
         temp = output
@@ -60,14 +68,13 @@ def clone_vmess(url):
         result = "vmess://" + vmess_url + "\n\n"
         clones.append(result)
 
+    # Save clones to results
     with open("data/vmess_clones.txt", "a") as data:
         for clone in clones:
             data.write(clone)
-
-
+            
 # Takes arguments
 arguments = sys.argv
-
 
 # Help message
 help_message = """
@@ -77,8 +84,7 @@ Help: python clones.py [[-h] [-vl url] [-vm url]]
     -vm url  : Clone vmess proxy
 """
 
-
-# Run the program
+# Main
 if __name__ == "__main__":
     if "-h" in arguments or "--help" in arguments:
         print(help_message)
@@ -93,5 +99,3 @@ if __name__ == "__main__":
 
     else:
         print("\033[2;31mError: Incorrect arguments! Use -h or --help\033[m")
-
-# EOF
