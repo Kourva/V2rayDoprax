947af43a5f94d045935e4e6a22e07945dd5e59bf
Kourva <118578799+Kourva@users.noreply.github.com>
2023-08-25 06:46:34 -0400

    Update getConfig.py

--- getConfig.py
+++ getConfig.py
@@ -1,32 +1,27 @@
 #!/usr/bin/env python3
-
+# -*- coding: utf-8 -*-
 
 # This script gets vmess config from vmess url
-
 
 # Imports
 import base64, time, json, sys
 
-
+# Printer
 def printer(message: str) -> None:
-    pretty_json = json.dumps(message, indent=4)
-    for char in pretty_json:
+    for char in json.dumps(message, indent=4):
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(0.01)
+    print()
 
-    print("\n")
-
-
-def get_config(vmess_url):
+# Get config from url
+def get_config(vmess_url: str) -> dict:
     encoded_data = vmess_url.split("vmess://")[1].strip()
-    output = json.loads(
+    return json.loads(
         base64.b64decode(encoded_data).decode("utf-8").replace("'", '"')
     )
-    return output
 
-
-# Run the program
+# Main
 if __name__ == "__main__":
     if len(sys.argv) != 1:
         if sys.argv[1] in ["-h", "--help"]:
@@ -41,7 +36,4 @@ Help: python getConfig.py [argumentss]
         else:
             printer(get_config(sys.argv[1]))
     else:
-        print("No argument! run with -h")
-
-
-# EOF
+        print("No argument specified! run with -h")
