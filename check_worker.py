d0c272acc3ea77b1ec90874e73c7f00c3139cb23
Kourva <118578799+Kourva@users.noreply.github.com>
2023-08-25 06:45:21 -0400

    Update check_worker.py

--- check_worker.py
+++ check_worker.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 # This Script checks your Worker service
 
 # Imports
 import requests, sys, bs4
 
-
 # Check the worker
 def check_worker(worker: str) -> None:
     try:
         print("\033[2;32mChecking...\033[m")
+
+        # Make get request to worker
         response = requests.get(worker, timeout=10)
         if response.status_code == 200:
             soup = bs4.BeautifulSoup(response.text, "html.parser")
             print(
-                "\033[2;32m"
-                + soup.find("p").text.replace("page", "message")
-                + f"\nStatus code:     {response.status_code}\033[m"
+                f"\033[2;32m"
+                f"soup.find("p").text.replace('page', 'message')"
+                f"\nStatus code:     {response.status_code}\033[m"
             )
+
+    # Handle exceptions
     except requests.exceptions.Timeout:
         print("\033[2;31mError: Timeout!! \033[m")
     except requests.exceptions.TooManyRedirects:
@@ -25,8 +29,7 @@ def check_worker(worker: str) -> None:
     except requests.exceptions.RequestException:
         print("\033[2;31mError: Request Exception!! \033[m")
 
-
-# Run the program
+# Main
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         if sys.argv[1] == "-h":
